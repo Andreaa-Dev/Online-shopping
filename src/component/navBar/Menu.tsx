4acdec26818aae8726cbd5862dcec443e4d905a4
Andrea <ngocanhhsgs@gmail.com>
2021-11-01 14:52:37 +0200

    update menu bar

---
+++ src/component/navBar/Menu.tsx
@@ -0,0 +1,17 @@
+import { Typography } from "@mui/material";
+import React from "react";
+
+function Menu() {
+  return (
+    <div>
+      <Typography>MAKEUP</Typography>
+      <Typography>COLLABS</Typography>
+      <Typography>SKINCARE</Typography>
+      <Typography>BODY</Typography>
+      <Typography>NEW ARRIVALS</Typography>
+      <Typography>BEST OF COLOURPOP</Typography>
+    </div>
+  );
+}
+
+export default Menu;

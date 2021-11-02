ef8a1ab8ddea3721d3c3e7c533dfd1dc00f72368
Andrea <ngocanhhsgs@gmail.com>
2021-11-02 14:14:11 +0200

    update sidebar

--- src/component/navBar/NavBar.tsx
+++ src/component/navBar/NavBar.tsx
@@ -1,41 +1,151 @@
-import React from "react";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-import AppBar from "@mui/material/AppBar";
+import * as React from "react";
+import {
+  createTheme,
+  styled,
+  ThemeProvider,
+  useTheme,
+} from "@mui/material/styles";
 import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import MuiAppBar, { AppBarProps as MuiAppBarProps } from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
+import CssBaseline from "@mui/material/CssBaseline";
+import List from "@mui/material/List";
 import Typography from "@mui/material/Typography";
-import DehazeIcon from "@mui/icons-material/Dehaze";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
 
-import SwitchTheme from "../navBar/SwitchTheme";
+import SwitchTheme from "./SwitchTheme";
 
-const theme = createTheme({
-  typography: {
-    fontFamily: ["Indie Flower", "cursive"].join(","),
-  },
-});
+const drawerWidth = 240;
+
+const Main = styled("main", { shouldForwardProp: (prop) => prop !== "open" })<{
+  open?: boolean;
+}>(({ theme, open }) => ({
+  flexGrow: 1,
+  padding: theme.spacing(3),
+  transition: theme.transitions.create("margin", {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  marginLeft: -drawerWidth,
+  ...(open && {
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: 0,
+  }),
+}));
+
+interface AppBarProps extends MuiAppBarProps {
+  open?: boolean;
+}
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== "open",
+})<AppBarProps>(({ theme, open }) => ({
+  transition: theme.transitions.create(["margin", "width"], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: drawerWidth,
+  }),
+}));
+
+const NavText = styled("div")(({ theme }) => ({
+  textAlign: "center",
+  margin: "auto",
+}));
+
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  justifyContent: "flex-between",
+  alignItems: "center",
+  padding: theme.spacing(0, 1),
+  ...theme.mixins.toolbar,
+}));
 
 export default function NavBar() {
-  return (
-    <ThemeProvider theme={theme}>
-      <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
-          <Toolbar>
-            <Box>
-              <DehazeIcon />
-            </Box>
-            <Typography
-              variant="h6"
-              noWrap
-              component="div"
-              sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
-            >
-              ColourPop
-            </Typography>
+  const themes = createTheme({
+    typography: {
+      fontFamily: ["Indie Flower", "cursive"].join(","),
+      fontSize: 25,
+    },
+  });
 
-            <SwitchTheme />
-          </Toolbar>
-        </AppBar>
-      </Box>
-    </ThemeProvider>
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar position="fixed" open={open}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="end"
+            onClick={handleDrawerOpen}
+            sx={{ ...(open && { display: "none" }) }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <ThemeProvider theme={themes}>
+            <NavText>
+              <Typography>COLOURPOP</Typography>
+            </NavText>
+          </ThemeProvider>
+          <SwitchTheme />
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          "& .MuiDrawer-paper": {
+            width: drawerWidth,
+          },
+        }}
+        variant="persistent"
+        anchor="left"
+        open={open}
+      >
+        <DrawerHeader>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === "rtl" ? (
+              <ChevronRightIcon />
+            ) : (
+              <ChevronLeftIcon />
+            )}
+          </IconButton>
+        </DrawerHeader>
+        <List>
+          {["MAKEUP", "COLLABS", "SKINCARE", "BODY"].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemText primary={text} style={{ fontWeight: "bold" }} />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+    </Box>
   );
 }
